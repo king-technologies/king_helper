764d6c1cc8401bc284021eefbefd038c30d9d4ab
garima498 <gmethi123@gmail.com>
2024-11-03 11:13:25 +0530

    Device Info Added

--- lib/king_helper.dart
+++ lib/king_helper.dart
@@ -3,6 +3,10 @@
 // Use of this source code is governed by a MIT license that can be
 // found in the LICENSE file.
 
+import 'dart:io';
+
 import 'package:device_info_plus/device_info_plus.dart';
+import 'package:flutter/foundation.dart';
 
 part 'src/device_info_methods.dart';
+part 'src/network_methods.dart';
---
+++ lib/src/network_methods.dart
@@ -0,0 +1,35 @@
+part of '../king_helper.dart';
+
+Future<String> getLocalSubnet() async {
+  try {
+    final ip = await getLocalIp();
+    return ip.split('.').sublist(0, 3).join('.');
+  } catch (e) {
+    debugPrint('Error getting local subnet: $e');
+    rethrow;
+  }
+}
+
+Future<String> getLocalIp() async {
+  try {
+    final interfaces =
+        await NetworkInterface.list(type: InternetAddressType.IPv4);
+    final supportedNetworks = ['wi-fi', 'wlan', 'ethernet'];
+    for (final interface in interfaces) {
+      var supported = false;
+      for (final network in supportedNetworks) {
+        if (interface.name.toLowerCase().contains(network)) {
+          supported = true;
+          break;
+        }
+      }
+      if (supported) {
+        return interface.addresses.first.address;
+      }
+    }
+  } catch (e) {
+    debugPrint('Error getting local IP: $e');
+    rethrow;
+  }
+  throw Exception('No devices found');
+}
